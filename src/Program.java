3058fb8b0cb86fd101b7a4440a6e7f9623fd8d9f
Marcel Riera <mrier011@fiu.edu>
2019-05-01 01:56:12 -0400

    Added support for short *.tm notation

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,8 @@
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -15,10 +17,12 @@ public class Program {
     public static final boolean LEFT = false;
     public static final boolean RIGHT = true;
 
+    private List<Instruction> instructions;
     private boolean[] direction;
     private Map<Character,Pair<Character,Integer>>[] instrMap;
     
     public Program() {
+        instructions = new ArrayList<>();
         direction = new boolean[PROGRAM_SIZE];
         instrMap = new Map[PROGRAM_SIZE];
     }
@@ -29,6 +33,7 @@ public class Program {
     }
     
     public void add(Instruction c) {
+        instructions.add(c);
         direction[c.state] = c.direction;
         if (instrMap[c.state] == null) 
             instrMap[c.state] = new HashMap<>();
@@ -57,31 +62,52 @@ public class Program {
     public void load(String filename) throws Exception {
         Scanner scanner = new Scanner(new File(INPUT_PATH + filename));
         
-        String fullSyntax = "^q([0-9]+)]([rl])\\((.)\\/(.),q(-?[0-9]+)\\)(#.*)?$";
-        String fastSyntax = "";
-        String shortcut = "\\((.),q(-?[0-9]+)\\)";
-        
-        Pattern pFull = Pattern.compile(fullSyntax);
-        
-        Pattern pFast = Pattern.compile(fastSyntax);
+        String fullNotation, fastNotation, fastSubNotation, shortcut;
+        fullNotation = "^q([0-9]+)]([rl])\\((.)\\/(.),q(-?[0-9]+)\\)(#.*)?$";
+        fastNotation = "^q([0-9]+)\\]([lr])(\\(.\\/.,q-?[0-9]+\\))*(#.*)?$";
+        fastSubNotation = "\\((.)\\/(.),q(-?[0-9]+)\\)"; // (n/m,q)
+        shortcut = "\\((.),q(-?[0-9]+)\\)"; // (#,q) -> (#/#,q)
+                
+        Pattern pFull = Pattern.compile(fullNotation);
+        Pattern pFast = Pattern.compile(fastNotation);
+        Pattern pSub  = Pattern.compile(fastSubNotation);
         
         while (scanner.hasNextLine()) {
             String currentLine = scanner.nextLine().replace(" ","");
             currentLine = currentLine.replaceAll(shortcut, "($1/$1,q$2)");
-            Matcher m = pFull.matcher(currentLine);
-            System.out.println(currentLine);
             
-            if (m.matches()) {
-                int s = Integer.parseInt(m.group(1));
-                boolean d  = m.group(2).equals("r") ? RIGHT : LEFT;
-                char t = m.group(3).charAt(0);
-                char r = m.group(4).charAt(0);
-                int n = Integer.parseInt(m.group(5));
+            Matcher m1 = pFull.matcher(currentLine);
+            Matcher m2 = pFast.matcher(currentLine);
+              
+            if (m1.matches()) {
+                int s = Integer.parseInt(m1.group(1));
+                boolean d  = m1.group(2).equals("r") ? RIGHT : LEFT;
+                char t = m1.group(3).charAt(0);
+                char r = m1.group(4).charAt(0);
+                int n = Integer.parseInt(m1.group(5));
                 this.add(new Instruction(s,d,t,r,n));
             }
-            
-            
+            else if (m2.matches()) {
+                int s = Integer.parseInt(m2.group(1));
+                boolean d  = m2.group(2).equals("r") ? RIGHT : LEFT;
+                
+                Matcher m3 = pSub.matcher(currentLine);
+                while (m3.find()) {
+                    char t = m3.group(1).charAt(0);
+                    char r = m3.group(2).charAt(0);
+                    int n = Integer.parseInt(m3.group(3));
+                    this.add(new Instruction(s,d,t,r,n));
+                }
+            }
         }
-    } 
+        System.out.println(this.toString());
+    }
+    
+    public String toString() {
+        String output = new String();
+        for (Instruction c : instructions)
+            output += c + "\n";
+        return output;
+    }
     
 }
--- src/Programs/Reverse.tm
+++ src/Programs/Reverse.tm
@@ -1,13 +1,1 @@
-q0]r (#/#, q-1)  # If nothing on input, return
-q0]r (1/1, q1)   # Check whether 1, 0 or #
-q0]r (0/0, q3)
-q1]r (1/1, q1)   # Traverse till end of tape
-q1]r (0/0, q1)
-q1]r (#/#, q2)
-q2]r (#/1, q5)   # Add one to end of tape
-q3]r (1/1, q3)   # Traverse till end of tape
-q3]r (0/0, q3)
-q3]r (#/#, q4)
-q4]r (#/0, q5)   # Add zero to end of tape
-q5]l (#/#, q-1)  
-q985] (1, q2)
+q0]r (1, q1)(2, q2) #test(#/1, q0)
